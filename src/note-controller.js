342789e2b7ede24acf42462caaced3dde752e124
Cornelis Heuperman <cheuperman@gmail.com>
2018-07-23 17:46:07 +0100

    Add controller that inserts note into view

---
+++ src/note-controller.js
@@ -0,0 +1,16 @@
+(function(exports) {
+  var noteList = new NoteList;
+
+  function NoteController(noteList) {
+    this.noteList = noteList;
+    this.noteList.enterNote("Favourite drink: seltzer");
+    this.noteListView = new NoteListView(noteList);
+  }
+
+NoteController.prototype.insertHTML = function () {
+  document.getElementById('app').innerHTML = this.noteListView.returnsHTML();
+  };
+
+  var noteController = new NoteController(noteList);
+  noteController.insertHTML();
+})(this);
